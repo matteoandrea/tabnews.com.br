6c0142abc341cb2963d349bb481164a37fc1aaf7
Matteo Andrea <61761339+matteoandrea@users.noreply.github.com>
2024-02-24 13:00:32 +0100

    added envirometn and database file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-export default status;
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
   response.status(200).send({ aluno: "AAAAAAAAAAAA" });
 }
+
+export default status;
